b6f388ea190f5900452cbc736ee558bb3cceadfc
Brandon Castro <bac522@nyu.edu>
2024-10-19 17:31:41 -0400

    Deal cards to player

--- src/clojure_blackjack/game.clj
+++ src/clojure_blackjack/game.clj
@@ -4,3 +4,14 @@
   (let [card (peek deck)
         deck (pop deck)]
     [card deck]))
+
+(defn deal-card->player [player deck]
+  (let [[card deck] (deal deck)]
+    [(update player :cards conj card) deck]))
+
+(defn reset-player-cards [player]
+  (assoc player :cards []))
+
+(defn player [player-type]
+  {:player-type player-type
+   :cards []})
--- test/clojure_blackjack/game_test.clj
+++ test/clojure_blackjack/game_test.clj
@@ -1,7 +1,8 @@
 (ns clojure-blackjack.game-test
   (:require
    [clojure-blackjack.card :refer [create-playing-deck]]
-   [clojure-blackjack.game :refer [deal]]
+   [clojure-blackjack.game :refer [deal deal-card->player player
+                                   reset-player-cards]]
    [clojure.test :refer [deftest is]]))
 
 (deftest deal-test
@@ -12,3 +13,37 @@
             :suit "spade"}
            card))
     (is (= 51 (count deck)))))
+
+(deftest deal-card-to-player-test
+  (let [player (player "player")
+        deck (vec (sort-by (juxt :suit :name)
+                           (create-playing-deck 1)))
+        [player deck] (deal-card->player player deck)]
+    (is (= {:player-type "player"
+            :cards [{:value 10
+                     :name "queen"
+                     :suit "spade"}]}
+           player))
+    (is (= 51 (count deck)))
+
+    (let [[player deck] (deal-card->player player deck)]
+      (is (= {:player-type "player"
+              :cards [{:value 10
+                       :name "queen"
+                       :suit "spade"}
+                      {:value 10
+                       :name "king"
+                       :suit "spade"}]}
+             player))
+      (is (= 50 (count deck))))))
+
+(deftest reset-player-cards-test
+  (let [player (player "player")
+        deck (vec (sort-by (juxt :suit :name)
+                           (create-playing-deck 1)))
+        [player _] (deal-card->player player deck)
+        player (reset-player-cards player)]
+    (is (= {:player-type "player"
+            :cards []}
+           player))))
+
