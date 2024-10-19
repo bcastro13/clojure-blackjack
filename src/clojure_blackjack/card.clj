9cdd8c296493fe8a5b56b433179e4ea7b6f82a54
Brandon Castro <bac522@nyu.edu>
2024-10-19 14:04:58 -0400

    Refactor card functions to be clearer

--- src/clojure_blackjack/card.clj
+++ src/clojure_blackjack/card.clj
@@ -12,28 +12,22 @@
 
 (defn create-card [card-name suit]
   (let [parsed-card-name (parse-int card-name)
-        value (if parsed-card-name
-                (if (and (< 1 parsed-card-name)
-                         (> 11 parsed-card-name))
-                  parsed-card-name
-                  (throw (Exception. "Invalid card type")))
-                (condp contains? card-name
-                  #{"jack" "queen" "king"} 10
-                  #{"ace"} 11
-                  (throw (Exception. "Invalid card type"))))]
+        value (cond
+                (and parsed-card-name (<= 2 parsed-card-name 10)) parsed-card-name
+                (contains? #{"jack" "queen" "king"} card-name) 10
+                (= "ace" card-name) 11
+                :else (throw (Exception. "Invalid card type")))]
     {:name card-name
      :suit suit
      :value value}))
 
 (defn create-full-suit [suit]
-  (let [full-suit (for [card-num (concat (range 2 11)
-                                         ["jack" "queen" "king" "ace"])]
-                    (create-card (str card-num) suit))]
-    full-suit))
+  (map #(create-card (str %) suit)
+       (concat (range 2 11)
+               ["jack" "queen" "king" "ace"])))
 
 (defn create-full-deck []
-  (flatten (map create-full-suit ["spade" "club" "diamond" "heart"])))
+  (mapcat create-full-suit ["spade" "club" "diamond" "heart"]))
 
 (defn create-playing-deck [num-decks]
-  (shuffle (flatten (for [_ (range num-decks)]
-                      (create-full-deck)))))
+  (shuffle (apply concat (repeat num-decks (create-full-deck)))))
